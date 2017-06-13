3547685803a688df353025dce27a5e5adaa63ca5
lixiang <csucoderlee@gmail.com>
2017-06-13 22:40:35 +0800

    项目新建

---
+++ src/main/java/com/csu/coderlee/ImoocbootApplication.java
@@ -0,0 +1,12 @@
+package com.csu.coderlee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImoocbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImoocbootApplication.class, args);
+	}
+}
