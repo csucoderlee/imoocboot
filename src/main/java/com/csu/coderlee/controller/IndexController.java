52b021a3003ce78994b76213af1e2cb167da24a3
lixiang <csucoderlee@gmail.com>
2017-06-14 08:39:29 +0800

    @controller 结合 thymeleaf来使用

---
+++ src/main/java/com/csu/coderlee/controller/IndexController.java
@@ -0,0 +1,17 @@
+package com.csu.coderlee.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by lixiang on 2017 06 14 上午8:33.
+ */
+@Controller
+public class IndexController {
+
+    @RequestMapping(value = "controller", method = RequestMethod.GET)
+    public String controller() {
+        return "index";
+    }
+}
