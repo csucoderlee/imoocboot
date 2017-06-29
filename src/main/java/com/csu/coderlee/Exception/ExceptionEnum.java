aaaf7ed7303c4182f8aad15fa7ae43999743c75b
lixiang <csucoderlee@gmail.com>
2017-06-29 17:13:06 +0800

    枚举隐式继承枚举类构造方法默认私有，最好不要setter

--- src/main/java/com/csu/coderlee/Exception/ExceptionEnum.java
+++ src/main/java/com/csu/coderlee/Exception/ExceptionEnum.java
@@ -10,7 +10,7 @@ public enum ExceptionEnum {
     private Integer code;
     private String message;
 
-    ExceptionEnum(Integer code, String message) {
+    private ExceptionEnum(Integer code, String message) {
 
         this.code = code;
         this.message = message;
@@ -21,15 +21,7 @@ public enum ExceptionEnum {
         return code;
     }
 
-    public void setCode(Integer code) {
-        this.code = code;
-    }
-
     public String getMessage() {
         return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
     }
 }
